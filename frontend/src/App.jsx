3610c62c0917afab2d6baa5642f733527d990a4d
RoushanKr <rk04393@gmail.com>
2025-09-26 02:21:30 +0530

    InitialFile For Frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <><h1>Welcome to Sweet Shop Management</h1><p>Your one-stop solution for managing your sweet shop.</p></>
+  )
+}
+
+export default App
