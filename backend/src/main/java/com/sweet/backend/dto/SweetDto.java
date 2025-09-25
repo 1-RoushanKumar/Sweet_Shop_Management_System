6411dda56bfacc320e5f510d1a23202ce26fc45f
RoushanKr <rk04393@gmail.com>
2025-09-25 23:13:09 +0530

    REFACTOR: Refactor Added Validation

--- backend/src/main/java/com/sweet/backend/dto/SweetDto.java
+++ backend/src/main/java/com/sweet/backend/dto/SweetDto.java
@@ -1,5 +1,9 @@
 package com.sweet.backend.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +12,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SweetDto {
+    @NotBlank(message = "Name is required")
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters")
     private String name;
+
+    @NotBlank(message = "Category is required")
     private String category;
-    private double price;
-    private int quantity;
+
+    @NotNull(message = "Price is required")
+    @Positive(message = "Price must be a positive value")
+    private Double price;
+
+    @NotNull(message = "Quantity is required")
+    @Positive(message = "Quantity must be a positive value")
+    private Integer quantity;
 }
