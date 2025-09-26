5263e13b4b02dda7ecc6dc1d543f109b22b59dd8
RoushanKr <rk04393@gmail.com>
2025-09-26 11:50:43 +0530

    Cross Orgin Connection on backend

--- backend/src/main/java/com/sweet/backend/controller/SweetController.java
+++ backend/src/main/java/com/sweet/backend/controller/SweetController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/sweets")
+@CrossOrigin(origins = "*")
 public class SweetController {
 
     private final SweetService sweetService;
